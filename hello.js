ee3713a016d749d92a8fb23d66bde9de543800a9
Courtney Amos <c.amos@shaw.ca>
2021-03-13 18:12:16 -0800

    Console log vs return examples

--- hello.js
+++ hello.js
@@ -5,3 +5,17 @@ const sayHello = function (name) {
 sayHello('Caliban');
 sayHello('Miranda');
 sayHello('Ferdinand');
+
+// console log
+const sayHelloToConsole = function (name) {
+  console.log('Hello, ' + name);
+};
+sayHelloToConsole('John');
+
+// return
+const returnSayHello = function (name) {
+  return 'Hello, ' + name;
+};
+const greeting = returnSayHello('John');
+// can console log later for output
+console.log(greeting);
