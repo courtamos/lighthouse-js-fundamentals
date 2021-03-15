80b393ce9b6183873b5b80dfa97ea41fb3e169c7
Courtney Amos <c.amos@shaw.ca>
2021-03-14 17:12:04 -0700

    Ice cream example for using array methods

---
+++ ice-cream.js
@@ -0,0 +1,21 @@
+const iceCreamFlavours = [
+  'chocolate',
+  'vanilla',
+  'cookies and cream',
+  'rocky road',
+  'strawberry',
+];
+console.log(iceCreamFlavours);
+
+// adding root beer as a flavour
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+
+// get first flavour
+console.log(iceCreamFlavours[0]);
+
+// get last flavour
+console.log(iceCreamFlavours[5]);
+
+// total number of flavours
+console.log(iceCreamFlavours.length);
