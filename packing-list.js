5a90a1f1b6a0a826ee2a61c756e4410ce9f99f1d
Courtney Amos <c.amos@shaw.ca>
2021-03-14 17:11:13 -0700

    Packing list using and array and while loop

--- packing-list.js
+++ packing-list.js
@@ -9,8 +9,18 @@ const packingList = [
   'cooking utensils',
 ];
 
+// for loop
 console.log('Kitchen stuff to pack:');
 
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+
+// while loop
+console.log('Kitchen stuff to pack:');
+
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
